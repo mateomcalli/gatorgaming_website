877299e7d1308dc12894724710429b9b9e5e8542
Mateo McAllister <mateomcallisterschool@gmail.com>
2025-06-07 21:33:31 -0400

    added first blur and line

--- website/src/pages/Home.jsx
+++ website/src/pages/Home.jsx
@@ -1,21 +1,26 @@
 import HomeButtons from '../components/HomeButtons'
+import BlurOne from '../bgblurs/BlurOne'
 
 const Home = () => {
   return (
-    <div className = 'flex w-2/3 h-screen'>
-      <div className='w-2/3 pt-10 sm:pt-15 md:pt-30 lg:pt-40 font-display sm:pl-2 md:pl-5 lg:pl-10'>
-        <p className='text-ggwelcome text-2xl pb-3'>
-          Welcome to Gator Gaming!
-        </p>
-        <h1 className='text-ggwhite font-bold text-5xl leading-tight'>
-          We're UF's student-led gaming club, committed to hosting community events that are completely free for all players.
-        </h1>
-        <p className='text-ggorange py-8 text-lg'>
-          Level Up Together: Your Ultimate Campus Gaming Community
-        </p>
-        <HomeButtons/>
+    <>
+      <div className = 'test-blue flex justify-between w-2/3 h-screen'>
+        <div className='w-2/3 pt-10 sm:pt-15 md:pt-30 lg:pt-40 font-display sm:pl-2 md:pl-5 lg:pl-10'>
+          <p className='text-ggwelcome text-2xl pb-3'>
+            Welcome to Gator Gaming!
+          </p>
+          <h1 className='text-ggwhite font-bold text-5xl leading-tight'>
+            We're UF's student-led gaming club, committed to hosting community events that are completely free for all players.
+          </h1>
+          <p className='text-ggorange py-8 text-lg'>
+            Level Up Together: Your Ultimate Campus Gaming Community
+          </p>
+          <HomeButtons/>
+        </div>
+        <div className = 'flex w-[3px] h-2/3 mt-22 sm:mr-2 md:mr-5 lg:mr-10 bg-ggwhite'></div>
       </div>
-    </div>
+      <BlurOne/>
+    </>
   )
 }
 
