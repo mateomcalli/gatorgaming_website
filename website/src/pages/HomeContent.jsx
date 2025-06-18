17eb6952d7a3758d687ba098c72f827493aa5f9d
Mateo McAllister <mateomcallisterschool@gmail.com>
2025-06-18 00:04:50 -0400

    should support up to ~900px width, check macbook

--- website/src/pages/HomeContent.jsx
+++ website/src/pages/HomeContent.jsx
@@ -4,17 +4,17 @@ import { FaArrowRightLong } from 'react-icons/fa6';
 const HomeContent = () => {
   return (
     <div className='justify-center flex z-2'>
-      <div className='grid grid-cols-2 grid-rows-[1fr_1fr] gap-x-15 gap-y-10 w-215 xl:w-315 2xl:w-332 h-screen z-1'>
-        <div className='flex flex-col gap-8 h-fit'>
+      <div className='grid grid-cols-2 grid-rows-[1fr_1fr] gap-x-15 xl:gap-y-10 w-215 xl:w-300 2xl:w-332 h-screen z-1'>
+        <div className='place-self-center xl:place-self-stretch flex flex-col gap-8 h-fit'>
           <img 
             className="aspect-auto z-2"
             src="home/home-players.png"
           />
         </div>
-        <div className='pl-10 justify-center flex flex-col z-1'>
-          <a href='#about-start'><p className='text-xl pb-3 text-ggwhite font-display scroll-m-30' id='about-start'>Who are we?</p></a>
-          <p className='text-3xl pb-2 font-bold text-ggwhite font-display'>We Always Make Epic Plays</p>
-          <p className='text-lg text-ggwhite font-display'>We’re a passionate gaming community dedicated to uniting players of all skill levels—whether you’re here to dominate tournaments, squad up with friends, or just have a good time. Join us and level up your gaming experience!</p>
+        <div className='xl:pl-10 justify-center flex flex-col z-1'>
+          <a href='#about-start'><p className='text-lg xl:text-xl pb-3 text-ggwhite font-display scroll-m-30' id='about-start'>Who are we?</p></a>
+          <p className='text-2xl xl:text-3xl pb-2 font-bold text-ggwhite font-display'>We Always Make Epic Plays</p>
+          <p className='text-md xl:text-lg text-ggwhite font-display'>We’re a passionate gaming community dedicated to uniting players of all skill levels—whether you’re here to dominate tournaments, squad up with friends, or just have a good time. Join us and level up your gaming experience!</p>
           <div className='flex gap-5 pt-8 font-display'>
             <a
               href="https://discord.com/invite/s99axhqQac"
@@ -28,43 +28,41 @@ const HomeContent = () => {
             </a>
           </div>
         </div>
-        <div className='z-2'>
-          <div className='pl-8'>
-            <p className='pb-2 text-2xl xl:text-3xl text-ggwhite font-display z-2'>
-            Our Skills
+        <div className='-mt-8 pl-6 xl:-mt-0 xl:pl-8 z-2'>
+          <p className='pb-2 text-2xl xl:text-3xl text-ggwhite font-display z-2'>
+          Our Skills
           </p>
           <p className='w-[95%] pb-2 text-md text-ggorange font-display'>
             We're known for tactical brilliance, unstoppable synergy, and fearless competition — our skills speak for themselves.
           </p>
           <SkillsList/>
-          </div>
         </div>
         <div className='flex flex-row text-center'>
-          <div className='pr-5 w-1/2 flex flex-col text-ggwhite font-display'>
-            <p className='text-2xl text-ggwhite font-display'>
+          <div className='pr-0 xl:pr-5 w-1/2 flex flex-col text-ggwhite font-display'>
+            <p className='text-xl xl:text-2xl text-ggwhite font-display'>
               Competition
             </p>
-            <p className='text-md px-5 pb-12 text-ggorange font-display'>
+            <p className='text-sm xl:text-md px-5 pb-12 text-ggorange font-display'>
               For the thrill of the game — from intense tournaments to friendly matches.
             </p>
-            <p className='text-2xl text-ggwhite font-display'>
+            <p className='text-xl xl:text-2xl text-ggwhite font-display'>
               Community
             </p>
-            <p className='text-md px-5 text-ggorange font-display'>
+            <p className='text-sm xl:text-md px-5 text-ggorange font-display'>
               A space where friendships are formed, online and off.
             </p>
           </div>
-          <div className='w-1/2 pl-5 flex flex-col text-ggwhite font-display'>
-            <p className='text-2xl text-ggwhite font-display'>
+          <div className='w-1/2 pr-0 xl:pl-5 flex flex-col text-ggwhite font-display'>
+            <p className='text-xl xl:text-2xl text-ggwhite font-display'>
               Collaboration
             </p>
-            <p className='text-md px-5 pb-12 text-ggorange font-display'>
+            <p className='text-sm xl:text-md px-5 pb-12 text-ggorange font-display'>
               Teamwork makes the dream work — we are always open to new ideas and input.
             </p>
-            <p className='text-2xl text-ggwhite font-display'>
+            <p className='text-xl xl:text-2xl text-ggwhite font-display'>
               Creativity
             </p>
-            <p className='text-md px-5 text-ggorange font-display'>
+            <p className='text-sm xl:text-md px-5 text-ggorange font-display'>
               Show off your skills, memes, montages, or cosplay.
             </p>
           </div>
