e148ebc8a6b68e02fef9f1fd2008c90f9f3a44f7
Mateo McAllister <mateomcallisterschool@gmail.com>
2025-06-29 01:22:20 -0400

    trying to fix blur

--- website/src/bgblurs/FaqBlur.jsx
+++ website/src/bgblurs/FaqBlur.jsx
@@ -2,7 +2,7 @@ const FaqBlur = () => {
   return (
     <>
       <img 
-        className='absolute top-20 scale-100 sm:top-8 lg:-top-35 lg:scale-80 md:right-20 md:-top-15 object-cover w-full xl:-top-55 xl:right-50 z-1 min-w-[400px] min-h-full' 
+        className='absolute top-20 scale-100 sm:top-8 lg:-top-35 lg:scale-80 md:right-20 md:-top-15 object-cover w-full xl:-top-30 2xl:-top-95 xl:right-50 z-1 min-w-[400px] min-h-full' 
         src='/home/faq-bg4.png' 
       />
     </>
--- website/src/components/footer/Footer.jsx
+++ website/src/components/footer/Footer.jsx
@@ -4,9 +4,9 @@ import { SiLinktree } from "react-icons/si";
 
 const Footer = () => {
   return (
-    <div className='mt-20 relative w-screen h-fit flex justify-center md:mt-70 lg:mt-20 xl:mt-0 z-2'>
+    <div className='mt-20 relative w-screen h-fit flex justify-center md:mt-70 lg:mt-20 xl:-mt-10 2xl:mt-0 z-2'>
       <img className='absolute inset-0 w-full h-full object-cover pointer-events-none overflow-hidden' src='footer-blur.png'/>
-      <div className='relative mx-8 w-full md:w-169 lg:w-205 xl:w-240 2xl:w-324 flex flex-col z-2'>
+      <div className='relative mx-8 w-full md:w-169 lg:w-205 xl:w-240 2xl:w-300 flex flex-col z-2'>
         <div className='flex h-fit w-full items-center gap-5 z-2'>
           <LuX size='40'/>
           <div className='h-[1px] w-[79%] xl:w-[83%] bg-ggwhite'/>
