157ba5678a1b219b79642d66461efb6999d746f1
Mateo McAllister <mateomcallisterschool@gmail.com>
2025-07-10 00:10:17 -0400

    added simplebar

--- website/src/pages/team/TeamContent.jsx
+++ website/src/pages/team/TeamContent.jsx
@@ -1,4 +1,6 @@
 import Card from "../../components/Card"
+import SimpleBar from 'simplebar-react'
+import 'simplebar-react/dist/simplebar.min.css';
 
 const TeamContent = () => {
   const members = [
@@ -13,13 +15,16 @@ const TeamContent = () => {
   return (
     <div className='relative pt-30 lg:pt-30 lg:pb-70 flex flex-col items-center w-screen min-h-screen'>
       <p className='relative text-4xl text-ggwhite font-display pb-10'>Meet our officers!</p>
-      <div className='test-green relative px-5 w-259.5 h-115 flex gap-x-12 !overflow-x-scroll'>
-        <div className='test-red bg-[linear-gradient(90deg,rgba(10,14,21,1)_0%,rgba(0,0,0,0)_100%)] absolute h-105 left-0 w-5'></div>
-        <div className='test-red bg-[linear-gradient(270deg,rgba(10,14,21,1)_0%,rgba(0,0,0,0)_100%)] absolute h-105 right-0 w-5'></div>
+      <SimpleBar 
+        className='h-120'
+        style={{ maxHeight: '460px', maxWidth: '1038px' }}
+      >
+        <div className='relative px-5 flex gap-x-12'>
           {members.map(member => (
-            <Card name={member.name} position={member.position}/>
+            <Card key={member.id} name={member.name} position={member.position}/>
           ))}
-      </div>
+        </div>
+      </SimpleBar>
     </div>
   )
 }
