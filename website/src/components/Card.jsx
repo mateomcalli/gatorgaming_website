a7bdcac72714cdb10e163b4c7fd3e778bf567e0b
mateomcallister <mateomcallisterschool@gmail.com>
2025-07-10 21:37:04 -0400

    flipping animation

--- website/src/components/Card.jsx
+++ website/src/components/Card.jsx
@@ -1,6 +1,14 @@
-const Card = ({ name, position }) => {
+import { motion } from 'framer-motion'
+
+const Card = ({ flipped, setFlipped, name, position }) => {
   return (
-    <div className='text-nowrap flex-shrink-0 flex flex-col rounded-xl w-75 h-105 border-solid border-8 border-ggorange bg-black'>
+    <motion.div
+      initial={{ rotateY: 0 }}
+      animate={{ rotateY: flipped ? 180 : 0 }}
+      transition={{ duration: 0.3 }}
+      onClick={() => setFlipped(!flipped)}
+      className='text-nowrap flex-shrink-0 flex flex-col rounded-xl w-75 h-105 border-solid border-8 border-ggorange bg-black'
+    >
       <div className='flex pb-1 justify-between pt-[3px] pl-[10px]'>
         <p className='text-lg font-bold font-display text-ggwhite'>{name}</p>
         <div className='flex gap-x-1 pr-1'>
@@ -15,7 +23,7 @@ const Card = ({ name, position }) => {
         <p className='pt-4 text-wrap text-ggorange text-lg font-display'>{position}</p>
       </div>
       <img className='pointer-events-none w-40 opacity-25 self-center -mt-0' src='gatorgaming-logo.png'/>
-    </div>
+    </motion.div>
   )
 }
 
--- website/src/pages/team/TeamContent.jsx
+++ website/src/pages/team/TeamContent.jsx
@@ -2,7 +2,7 @@ import Card from "../../components/Card"
 import SimpleBar from 'simplebar-react'
 import 'simplebar-react/dist/simplebar.min.css'
 import { useDrag } from '@use-gesture/react'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
 const TeamContent = () => {
   const scrollRef = useRef(null)
@@ -28,13 +28,19 @@ const TeamContent = () => {
   return (
     <div className='relative pt-30 lg:pt-30 lg:pb-70 flex flex-col items-center w-screen min-h-screen'>
       <p className='relative text-4xl text-ggwhite font-display pb-10'>Meet our officers!</p>
-      <SimpleBar className='h-115 max-w-259.5' ref={scrollRef}>
-        <div className={`relative px-80 sm:px-60 md:px-45 lg:px-5 flex gap-x-12 w-fit select-none cursor-grab active:cursor-grabbing`} {...bind()}>
-          {members.map((member, index) => (
-            <Card key={index} name={member.name} position={member.position}/>
-          ))}
-        </div>
-      </SimpleBar>
+      <div className='relative'>
+        <SimpleBar className='h-115 w-screen lg:w-259.5 max-w-259.5' ref={scrollRef}>
+          <div className='relative px-10 lg:px-5 flex gap-x-12 w-fit select-none cursor-grab active:cursor-grabbing' {...bind()}>
+            {members.map((member, index) => {
+              const [flipped, setFlipped] = useState(false)
+              return (
+                <Card flipped={flipped} setFlipped={setFlipped} key={index} name={member.name} position={member.position}/>
+            )})}
+          </div>
+        </SimpleBar>
+        <div className='absolute left-0 top-0 h-full w-6 bg-[linear-gradient(90deg,rgba(10,14,21,1)_0%,rgba(0,0,0,0)_100%)] pointer-events-none z-2'></div>
+        <div className='absolute right-0 top-0 h-full w-10 bg-[linear-gradient(270deg,rgba(10,14,21,1)_0%,rgba(0,0,0,0)_100%)] pointer-events-none z-2'></div>
+      </div>
     </div>
   )
 }
