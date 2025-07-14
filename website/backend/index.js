2313faf2fbc6a6fa410c82c6bc3dacd72ca984b6
Mateo McAllister <mateomcallisterschool@gmail.com>
2025-07-14 18:40:59 -0400

    added functionality to add event to local server through frontend

--- website/backend/index.js
+++ website/backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+app.use(cors()) // necessary to send req from frontend
+app.use(express.json()) // necessary to parse json data
 const port = 3000
 
-const events = [
+let events = [
   {
     id: '1',
     title: "Smash Bros Tournament",
@@ -40,6 +43,20 @@ app.get('/api/events/:id', (req, res) => {
   else {
     res.status(404).json({ error: 'event not found' })
   }
+})
+
+app.post('/api/events/', (req, res) => {
+  const body = req.body
+  
+  const event = {
+    title: body.title,
+    location: body.location,
+    date: body.date,
+    time: body.time
+  }
+
+  events = events.concat(event)
+  res.json(event)
 })
 
 app.listen(port, () => {
--- website/src/pages/admin/Admin.jsx
+++ website/src/pages/admin/Admin.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 const Admin = () => {
   const [eventData, setEventData] = useState(
@@ -10,8 +10,17 @@ const Admin = () => {
       time: '',
     }
   )
+  const [showSuccess, setShowSuccess] = useState(false)
+  const formRef = useRef(null)
 
   const url = 'http://localhost:3000/api/events'
+
+  const successMessage = () => {
+    setShowSuccess(true)
+    setTimeout(() => {
+      setShowSuccess(false)
+    }, '1500')
+  }
 
   const handleChange = (event) => {
     event.preventDefault()
@@ -24,18 +33,32 @@ const Admin = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    axios.post(url, )
+    axios
+      .post(url, eventData)
+      .then(response => {
+        successMessage()
+        setEventData(
+          {
+            title: '',
+            location: '',
+            date: '',
+            time: '',
+          })
+        formRef.current.reset()
+        return response.data
+      })
+    
   }
   return (
     <div className='relative pt-30 lg:pb-70 flex flex-col items-center w-screen min-h-screen'>
       <div className='w-60 px-3 pt-2 pb-5 rounded-xl bg-ggbg drop-shadow-sm drop-shadow-ggorange'>
         <p className='font-display text-ggorange pb-2'>add a new event:</p>
-        <form className='flex flex-col' onSubmit={handleSubmit}>
+        <form className='flex flex-col' ref={formRef} onSubmit={handleSubmit}>
           <input className='font-display focus:outline-none' name='title' placeholder="title" onChange={handleChange}/>
           <input className='font-display focus:outline-none' name='location' placeholder="location" onChange={handleChange}/>
           <input className='font-display focus:outline-none' type='date' name='date' onChange={handleChange}/>
           <input type='time' name='time' onChange={handleChange}/>
-          <button type='submit' className='w-fit px-2 self-center font-display cursor-pointer mt-3 rounded-4xl border-2 border-ggorange'>add event!</button>
+          <button type='submit' className={`w-fit px-2 self-center font-display cursor-pointer mt-3 rounded-4xl border-2 ${showSuccess ? 'border-green-800': 'border-ggorange'}`}>{showSuccess ? 'submitted!' : 'add event!'}</button>
         </form>
       </div>
     </div>
